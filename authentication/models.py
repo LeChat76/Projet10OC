ec6833280917f648437a059216f7d4d05078903d
LeChat76 <cedrik76@msn.com>
2023-07-05 15:13:07 +0200

    maj README.md

--- authentication/models.py
+++ authentication/models.py
@@ -6,6 +6,10 @@ class Users(AbstractUser):
     can_be_contacted = models.BooleanField(default=False)
     can_data_be_shared = models.BooleanField(default=False)
     birthday = models.DateField()
+    username = models.CharField(
+        max_length = 20,
+        unique = True,
+    )
 
     def is_adult(self):
         today = date.today()
