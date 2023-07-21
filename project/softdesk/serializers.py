a0980bc37214198827b1957fefec4afb8dddbe51
LeChat76 <cedrik76@msn.com>
2023-07-21 14:58:12 +0200

    .

--- project/softdesk/serializers.py
+++ project/softdesk/serializers.py
@@ -48,7 +48,7 @@ class IssueListSerializer(ModelSerializer):
 
 class IssueDetailSerializer(ModelSerializer):
 
-    user = serializers.IntegerField(default=serializers.CurrentUserDefault(), write_only=True)
+    user = serializers.IntegerField(write_only=True)
     
     class Meta:
         model = Issue
