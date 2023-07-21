0d85e390eafd2f4ddda05b1f7c035f487e6dbda7
LeChat76 <cedrik76@msn.com>
2023-07-21 15:16:08 +0200

    .

--- project/softdesk/views.py
+++ project/softdesk/views.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from rest_framework.status import status
+from rest_framework import status
 
 from .models import Contributor, Project, Issue, Comment
 from .serializers import ProjectListSerializer, ProjectDetailSerializer
